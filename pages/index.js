66517208000cefac07f413ca6b8f7d87b10559d8
Alexandre Gérault <alexandre@hexium.io>
2020-11-03 20:19:41 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My awesome Datepicker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
